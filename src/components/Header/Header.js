5e54e84b35bf4b3eb85feb9baf8fee7299009673
brianwmorgan <brianwmorgan317@gmail.com>
2023-02-09 12:40:26 -0500

    refactor header using SASS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import roosterGrinLogo from "../../assets/images/logo-rooster-grin-white.svg";
-import "./Header.css";
+// import "./Header.css";
+import "./Header.scss";
 
 function Header() {
   return (
     <div className="container header" id="header">
       <div className="row">
         <div className="col">
-          <img className="rooster-grin-logo" src={roosterGrinLogo} alt="logo" />
+          <img className="header__rooster-grin-logo" src={roosterGrinLogo} alt="logo" />
           <div className="welcome-text">
             <h1 className="welcome-to">WELCOME TO</h1>
             <h1 className="rooster-grin">Rooster Grin</h1>
